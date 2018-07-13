115739cc4c0b05e777a1e7da03a6522f8c8442a7
jaketaylor41 <jacobdeantaylor@gmail.com>
2018-07-13 16:18:13 -0700

    Fixed 404 Error

--- src/main/resources/static/client.js
+++ src/main/resources/static/client.js
@@ -87,56 +87,65 @@ function enabled() {
             "method": "GET",
             "cache": "false",
             "headers": {},
-            "data": "{}"
+            "data": "{}",
+            "error": function (jqXHR, textStatus){
 
+                if (jqXHR.status === 404) {
+
+                    var imgArray = new Array();
+                    imgArray[0] = "images/batman.jpg";
+                    imgArray[1] = "images/godfather.jpg";
+                    imgArray[2] = "images/shawshank.jpg";
+                    imgArray[3] = "images/avengers.jpg";
+                    imgArray[4] = "images/pulp.jpg";
+                    imgArray[5] = "images/goodfellas.jpg";
+                    imgArray[6] = "images/inception.jpg";
+                    imgArray[7] = "images/forrestgump.jpg";
+                    imgArray[8] = "images/matrix.jpg";
+                    imgArray[9] = "images/fightClub.jpg";
+                    imgArray[10] = "images/theGood.jpg";
+                    imgArray[12] = "images/city.jpg";
+                    imgArray[13] = "images/departed.jpg";
+                    imgArray[14] = "images/greenMile.jpg";
+                    imgArray[15] = "images/psycho.jpg";
+                    imgArray[16] = "images/spirited.jpg";
+                    imgArray[17] = "images/twelveAngryMen.jpg";
+                    imgArray[18] = "images/interstellar.jpg";
+                    imgArray[19] = "images/schindler.jpg";
+                    imgArray[20] = "images/back2Future.jpg";
+
+                    var rnd = Math.floor(Math.random() * imgArray.length);
+                    if (rnd == 0) {
+                        rnd = 1;
+                    }
+
+                    $(".movieContainer").append('<img id="movieImg" style="padding-top: 20px;" src="' + imgArray[rnd] + '"/>').hide().fadeIn(2000);
+                    $(".movieContainer").append('<img id="backdrop"  style="position: absolute; margin: 0 auto; display: block; width: 100%; height: auto; z-index: -1000; top: 0; opacity: .6; border-radius: 0; animation: none;" src="' + imgArray[rnd] + '"/>').hide().fadeIn(2000);
+
+                    // $(".movieContainer").append('<h1 class="welcomeIntro" style="text-align: center; transform: translateY(200%); margin: 0; font-family: SF-Pro-Display-Medium;">Bad Deadpool... Good Deadpool!</h1>');
+                    // $(".movieContainer").append('<img src="images/deadpool.jpg" style="width: 45%; position: absolute; margin-left: 20%; bottom: 0;">')
+                }
+
+            }
         };
 
         $.ajax(settings).done(function (response) {
             console.log(response);
-                $(".movieContainer").append('<img id="movieImg" onerror="brokenImage()" style="padding-top: 20px;" src="http://image.tmdb.org/t/p/original' + response.poster_path + '"/>').hide().fadeIn(2000);
-                $(".movieContainer").append('<img id="backdrop" onerror="brokenImage()" style="position: absolute; margin: 0 auto; display: block; width: 100%; height: auto; z-index: -1000; top: 0; opacity: .6; border-radius: 0; animation: none;" src="http://image.tmdb.org/t/p/original' + response.backdrop_path + '"/>');
-                $(".movieContainer").append('<a href="https://www.imdb.com/title/' + response.imdb_id +'" target="_blank"><span style="margin: 10px 0 10px 0; font-size: 25px; position: relative; z-index: 5000; display: inline-block;" class="glyphicon glyphicon-info-sign"></span></a>');
+                $(".movieContainer").show('<img src="images/ripple.gif">');
+                $(".movieContainer").append('<img id="movieImg" style="padding-top: 20px;" src="http://image.tmdb.org/t/p/original' + response.poster_path + '"/>').hide().fadeIn(2000);
+                $(".movieContainer").append('<img id="backdrop" style="position: absolute; margin: 0 auto; display: block; width: 100%; height: auto; z-index: -1000; top: 0; opacity: .6; border-radius: 0; animation: none;" src="http://image.tmdb.org/t/p/original' + response.backdrop_path + '"/>').hide().fadeIn(2000);
+                $(".movieContainer").append('<a href="https://www.imdb.com/title/' + response.imdb_id +'" target="_blank"><span style="margin: 10px 0 10px 0; font-size: 25px; position: relative; z-index: 5000; display: inline-block;" class="glyphicon glyphicon-info-sign"></span></a>').hide().fadeIn(2000);
 
         });
-        $(".movieContainer").empty();
+        $(".movieContainer").empty('<img src="images/ripple.gif">');
         $(".movieContainer").empty();
         $(".welcomeIntro").empty();
         $(".welcomeImg").remove();
-
-        function brokenImage(){
-            $('img').error(function(){
-                $(this).attr('src', 'deadpool.jpg');
-            });
-
-        }
-
     });
+
 }
 
 
-    function actorCarousel() {
-
-        $('#recipeCarousel').carousel({
-            interval: 1000,
-        });
-
-        $('.carousel .carousel-item').each(function () {
-            var next = $(this).next();
-            if (!next.length) {
-                next = $(this).siblings(':first');
-            }
-            next.children(':first-child').clone().appendTo($(this));
-            for (var i = 0; i < 2; i++) {
-                next = next.next();
-                if (!next.length) {
-                    next = $(this).siblings(':first');
-                }
-
-                next.children(':first-child').clone().appendTo($(this));
-            }
-        });
-
-    }
 
 $("#homeBtn").click(function(event) {
     event.preventDefault(event);
